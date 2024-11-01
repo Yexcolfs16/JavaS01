3480f1b0abfa457236f1810faf455c177634bab9
Juarez Perez Wayner <wayner.juarez@grupopakar.com.mx>
2024-10-31 18:11:50 -0600

    funcionesBasicas

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,14 @@
+function saludar(){
+    console.log('Hola mundo');
+    
+}
+
+saludar();
+
+
+function suma(){
+    return 2 + 2;
+}
+
+//let resultado = suma(); se crfeo la variable para darle el resultado de la function
+console.log(suma());
