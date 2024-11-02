7561bfa16e0d314e60fcdf4e5a57ec1ea9ac930d
Juarez Perez Wayner <wayner.juarez@grupopakar.com.mx>
2024-11-01 18:28:19 -0600

    operadorAritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+
+//Declaraciones
+let a = 5; 
+let b = 7;
+
+// console.log(a + b, 'adición');        // adicción
+// console.log(a - b,'resta');           // resta
+// console.log(a * b, 'multiplicación'); // multiplicación
+// console.log(a / b), 'división';       // división
+// console.log(a % b, 'modulo');         // módulo
+// console.log(a ** b, 'potencia');      // potencia
+
+//incremento 
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
+
